831ddc84933e941ad59d4c2ded1496d171f54d04
mefool <vicentegarciafernandes@tecnico.ulisboa.pt>
2020-07-13 22:38:48 +0000

    problem 034 solved in c++ Digit Factorials

---
+++ cpp_euler/20-40/p034.cpp
@@ -0,0 +1,28 @@
+/* Project Euler - Problem 34
+  Digit factorials
+
+  p034.exe
+  g++ p034.cpp -o p034.exe
+  
+*/
+#include <iostream>
+#include <string>
+
+int main(/*int argc, char *argv[]*/) {
+  int factorials[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+
+  int result = 0;
+
+  for (int i = 3; i<10E7; i++){ // 1! and 2! are not within the interval
+    int sum = 0;
+    int n = i;
+    while (n > 0){
+      sum += factorials[n % 10];
+      n /= 10;
+    }
+    if (i == sum)
+      result += i;
+  }
+  std::cout << "Sum: " << result << std::endl;
+  return 0;
+}
