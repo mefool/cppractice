e251c3f178b16227211021fc2a41fb29e3d5c8c0
mefool <vicentegarciafernandes@hotmail.com>
2022-11-26 13:57:23 +0000

    problem 024 improved in julia
    used nthperm(a, n) instead of permutations(a)[n]

--- julia_euler/20-40/p024.jl
+++ julia_euler/20-40/p024.jl
@@ -1,7 +1,18 @@
 ### Problem 24 - https://projecteuler.net/problem=24
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?"
+# reference: https://juliamath.github.io/Combinatorics.jl/dev/api/#Permutations
+# todo: check nthperm(a, n) function compared to permutations (time and allocations)
 
 using Combinatorics
 
 println("example: ", collect(permutations(0:2)))
-println("answer: ", collect(permutations(0:9))[10^6])
+println("answer: ", nthperm(0:9,10^6))
+
+if true
+    print("@time collect(permutations(0:9))[10^6]:")
+    an_perm = @time collect(permutations(0:9))[10^6]
+    print("@time nthperm(0:9,10^6):               ")
+    an_nperm = @time nthperm(0:9,10^6)
+    println("collect(permutations(0:9))[10^6]: ", an_perm)
+    println("nthperm(0:9,10^6):                ", an_perm)
+end
